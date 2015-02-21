54ee4ca0d42830f10a558efba90936a56efbe732
AVONnadozie <anuebunwa.victor@gmail.com>
2015-02-22 00:06:46 +0100

    Added Metro UI CSS files, Website template and prototype
    Added Metro UI CSS files( which includes Bootstrap CSS files), website
    template, header file, footer file and website protype. Also modified
    the index.php file to include the added CSS files, header file and
    footer file.

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="bg-dark">
+    <div class="container tertiary-text bg-dark fg-white" style="padding: 10px">
+        &copy; <a href="<?=$GLOBALS["ndg_homepage"]?>" class="fg-white fg-hover-yellow fg-active-amber">NACOSS UNN Developers Group (NDG)</a>
+        <div class="place-right compact">
+            <a href="#" class="fg-white fg-hover-yellow fg-active-amber"> FeedBack </a>
+            &nbsp;|&nbsp;
+            <a href="#" class="fg-white fg-hover-yellow fg-active-amber"> FAQs</a>
+        </div>
+    </div>
+</div>
---
+++ header.php
@@ -0,0 +1,51 @@
+<?php include_once './constants.php';?>
+<div class="">
+    <div class="navigation-bar bg-NACOSS-UNN-green">
+        <div class="container">
+            <div class="navigation-bar-content">
+                <a class="element  bg-hover-dark" href="index.php">NACOSS UNN</a>
+                <span class="element-divider"></span>
+                <a class="element1 pull-menu" href="#"></a>
+                <ul class="element-menu">
+                    <li class="on-phone on-tablet no-desktop">
+                        <a class="bg-hover-dark" href="#">Profile</a>
+                    </li>
+                    <li>
+                        <a class="bg-hover-dark" href="#">Forum</a>
+                    </li>
+                    <li>
+                        <a class="bg-hover-dark" href="#">Prospectus</a>
+                    </li>
+                    <li>
+                        <a class="bg-hover-dark" href="#">Library</a>
+                    </li>
+                    <li>
+                        <a class="bg-hover-dark" href="#">Alumni</a>
+                    </li>
+                    <li>
+                        <a class="bg-hover-dark" href="#">Contact</a>
+                    </li>
+
+                </ul>
+
+                <div class="no-phone no-tablet">
+
+                    <span class="element-divider place-right"></span>
+                    <button class="element bg-transparent bg-hover-dark place-right">
+                        <i class="icon-user-2"></i>
+                        John Doe
+                    </button>
+<!--
+                    <span class="element-divider place-right"></span>
+
+                    <a class="element place-right fg-lightRed bg-hover-lightRed fg-hover-white" href="#" target="_blank">
+                        <i class="icon-google-plus"></i>
+                    </a>
+                    <a class="element place-right fg-darkBlue bg-hover-darkBlue fg-hover-white" href="#" target="_blank">
+                        <i class="icon-facebook"></i>
+                    </a>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
