db2a126ffe8ea95dc145c1bd97b8af94aa24d159
AVONnadozie <anuebunwa.victor@gmail.com>
2015-03-14 14:15:02 +0100

    Added links for FeedBack and FAQs

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
     <div class="container tertiary-text bg-dark fg-white" style="padding: 10px">
         &copy; <a href="<?=$GLOBALS["ndg_homepage"]?>" class="fg-white fg-hover-yellow fg-active-amber">NACOSS UNN Developers Group (NDG)</a>
         <div class="place-right compact">
-            <a href="#" class="fg-white fg-hover-yellow fg-active-amber"> FeedBack </a>
+            <a href="contact.php" class="fg-white fg-hover-yellow fg-active-amber"> FeedBack </a>
             &nbsp;|&nbsp;
-            <a href="#" class="fg-white fg-hover-yellow fg-active-amber"> FAQs</a>
+            <a href="faq.php" class="fg-white fg-hover-yellow fg-active-amber"> FAQs</a>
         </div>
     </div>
 </div>
