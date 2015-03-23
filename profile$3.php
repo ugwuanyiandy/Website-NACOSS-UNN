5e2173d6922c41d47dac01a362dc58ccaef2f295
AVONnadozie <anuebunwa.victor@gmail.com>
2015-03-23 03:20:12 +0100

    Added results page

--- profile$3.php
+++ profile$3.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * Copyright 2015 NACOSS UNN Developers Group (NDG).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,12 +16,42 @@
  */
 
 require_once './functions.php';
+$array = getResults();
 ?>
 <div>
     <h2>Results</h2>
     <div class="row bg-grayLighter">
-        <div class="padding5">
-            
+        <div class="padding5 grid">
+            <?php if (empty($array)) { ?>
+                <h3>No results available.</h3>
+                <?php
+            } else {
+                $course = "";
+                foreach ($array as $result) {
+                    if (strcasecmp($course, $array['course_code']) !== 0) {
+                        if (!empty($course)) {
+                            //If not the first course dispayed, close a div
+                            echo '</div>';
+                        }
+                        //Display header
+                        echo '<h3>' . $array['course_code'] . ', '
+                        . 'Semester: ' . $array['semester'] . ', '
+                        . 'Year: ' . $array['year'] . '</h3>';
+                        //Open a row for images
+                        echo '<div class="row">';
+                        $course = $array['course_code'];
+                    }
+                    ?>
+                    <!--Display Image-->
+                    <div class="image-container">
+                        <img class="image span4" src="<?= $array['img_url'] ?>" alt=""/>
+                        <div class="overlay-fluid">page <?= $array['page_no'] ?></div>
+                    </div>
+                    <?php
+                }
+                echo '</div>';
+            }
+            ?>
         </div>
     </div>
 </div>
