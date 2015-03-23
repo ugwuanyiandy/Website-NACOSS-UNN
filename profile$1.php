9f8fbdecaf8befef202e2ec4da663667d53e9997
AVONnadozie <anuebunwa.victor@gmail.com>
2015-03-23 03:19:39 +0100

    Added user profile page

--- profile$1.php
+++ profile$1.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * Copyright 2015 NACOSS UNN Developers Group (NDG).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,9 +31,89 @@ $array = getUserData();
             Edit Profile
         </a>
     </div>
-    <div class="row bg-grayLighter">
-        <div class="padding5">
-            
+    <div class="row ntp ntm">
+        <div class="">
+            <div class="grid">
+                <div class="row">
+                    <div class="span2 no-phone no-tablet bg-grayLighter">
+                        <img class="image shadow padding5" src="<?=
+                        isset($array['pic_url']) && !empty($array['pic_url']) ?
+                                $array['pic_url'] :
+                                "img/picture5.png"
+                        ?>" alt=""/>
+                        <br/>
+                        <button class="button bg-NACOSS-UNN bg-hover-dark fg-hover-white file" style="width: inherit;">
+                            Change Picture
+                        </button>
+                    </div>
+                    <div class="span7 bg-grayLighter shadow">
+                        <div class="padding10">
+                            <!--Name-->
+                            <h2>
+                                <?php
+                                echo strtoupper($array['first_name']) . " ";
+                                echo empty($array['last_name']) ? "" : strtoupper($array['other_names']) . " ";
+                                echo strtoupper($array['last_name'])
+                                ?>
+                            </h2>
+                            <!--Registration number-->
+                            <p><?= $array['regno'] ?> 
+                                <!--Show verification status-->
+                                <?php
+                                if (isset($array['verified']) && $array['verified'] == 1) {
+                                    echo '<i title="Verified" class="icon-checkmark fg-NACOSS-UNN"></i>';
+                                }
+                                ?>
+                            </p>
+                            <!--Department and level-->
+                            <?php
+                            if (isset($array['department']) && !empty($array['department'])) {
+                                echo "Department of ".ucwords($array['department']) . " ";
+                                echo ucwords($array['level']);
+                                echo '<br/>';
+                                echo 'Class of '.$array['entry_year'];
+                                echo '<br/>';
+                            }
+                            ?>
+                            <!--Email-->
+                            <p class="email"><?= $array['email'] ?></p>
+                            <!--Bio-->
+                            <?php
+                            if (isset($array['bio']) && !empty($array['bio'])) {
+                                echo $array['bio'];
+                                echo '<br/>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row ntm">
+        <div class="">
+            <div class="panel no-border bg-transparent" data-role="panel">
+                <p class="panel-header">Personal Information</p>
+                <div class="panel-content bg-grayLighter">
+                    <p><strong>Date of Birth:</strong> <?= $array['dob']?></p>
+                </div>
+            </div>
+            <br/>
+            <div class="panel no-border bg-transparent" data-role="panel">
+                <p class="panel-header">Contact Information</p>
+                <div class="panel-content bg-grayLighter">
+                    <p><strong>Phone:</strong> <?= $array['phone']?></p>
+                    <p><strong>Address 1:</strong> <?= $array['address1']?></p>
+                    <p><strong>Address 1:</strong> <?= $array['address2']?></p>                    
+                </div>
+            </div>
+            <br/>
+            <div class="panel no-border bg-transparent" data-role="panel">
+                <p class="panel-header">Interests/Activities</p>
+                <div class="panel-content bg-grayLighter">
+                    <p><strong>Interest:</strong> <?= $array['interest']?></p>                    
+                </div>
+            </div>
         </div>
     </div>
 </div>
