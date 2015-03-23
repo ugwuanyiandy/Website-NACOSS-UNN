f732989176a41405e35ab5c005893c4f98d49480
AVONnadozie <anuebunwa.victor@gmail.com>
2015-03-23 03:17:44 +0100

    Modified footer color

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="bg-dark">
     <div class="container tertiary-text bg-dark fg-white" style="padding: 10px">
-        &copy; <a href="<?=$GLOBALS["ndg_homepage"]?>" class="fg-white fg-hover-yellow fg-active-amber">NACOSS UNN Developers Group (NDG)</a>
+        &copy; <a href="<?= $GLOBALS["ndg_homepage"] ?>" class="fg-white fg-hover-yellow fg-active-amber">NACOSS UNN Developers Group (NDG)</a>
         <div class="place-right compact">
             <a href="contact.php" class="fg-white fg-hover-yellow fg-active-amber"> FeedBack </a>
             &nbsp;|&nbsp;
